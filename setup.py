a38fc204592f6882a74a5e0371782cd00710501d
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-12-06 20:11:57 +0000

    1.0.0

--- setup.py
+++ setup.py
@@ -6,35 +6,23 @@ from setuptools import find_packages
 from setuptools import setup
 import shlex
 import subprocess
-# import sys
+import sys
 
 
-def git_hash():
-    cmd = 'git log -1 --format="%h"'
-    try:
-        hash_ = subprocess.check_output(shlex.split(cmd)).decode().strip()
-    except subprocess.CalledProcessError:
-        hash_ = None
-    return hash_
+version = '1.0.0'
 
 
-version = '0.2.0-0'
-
-hash_ = git_hash()
-if hash_ is not None:
-    version = '%s.%s' % (version, hash_)
-
-
-# # release helper
-# if sys.argv[-1] == 'release':
-#     commands = [
-#         'python setup.py sdist upload',
-#         'git tag v{0}'.format(version),
-#         'git push origin master --tag',
-#     ]
-#     for cmd in commands:
-#         subprocess.check_call(shlex.split(cmd))
-#     sys.exit(0)
+# release helper
+if sys.argv[-1] == 'release':
+    commands = [
+        'python setup.py sdist',
+        'git tag v{0}'.format(version),
+        'git push origin --tags',
+        'twine upload screenshot_manager-{0}.tar.gz'.format(version),
+    ]
+    for cmd in commands:
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
 
 
 setup(
